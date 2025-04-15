815c9c64f4b253d451a61bab27f1c959b2afdad8
Jesse Zahrt <jdzahrt@gmail.com>
2025-04-15 05:12:46 -0500

    Hook up api

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,25 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Screen from "../components/Screen";
 import {FlatList, StyleSheet} from "react-native";
 import Card from "../components/Card";
 import colors from "../config/colors";
 import routes from "../navigation/routes";
+import {getListings} from "../api/listings";
 
-const listings = [
-    {
-        id: 1,
-        title: 'Red jacket for sale',
-        price: 100,
-        image: require('../assets/jacket.jpg')
-    }, {
-        id: 2,
-        title: 'Chair that is in decent shape',
-        price: 200,
-        image: require('../assets/chair.jpg')
-    }
-]
 
 function ListingsScreen({navigation}) {
+    const [listings, setListings] = useState()
+
+    useEffect(() => {
+        loadListings();
+    }, []);
+
+    const loadListings = async () => {
+        const response = await getListings();
+        setListings(response.data);
+    }
+
     return (
         <Screen style={styles.screen}>
             <FlatList
@@ -29,7 +28,7 @@ function ListingsScreen({navigation}) {
                     <Card
                         title={item.title}
                         subTitle={'$' + item.price}
-                        image={item.image}
+                        imageUrl={item.images[0].url}
                         onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
                     />
                 }
